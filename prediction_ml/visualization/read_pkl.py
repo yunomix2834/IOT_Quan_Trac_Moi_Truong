470fe877b4a490654d34f4fb3677ca632f159702
Yunomi Xavia <yunomix2834@gmail.com>
2024-09-04 00:47:23 +0700

    update

--- prediction_ml/visualization/read_pkl.py
+++ prediction_ml/visualization/read_pkl.py
@@ -1,7 +1,7 @@
 import joblib
 
 # Đường dẫn tới file .pkl
-file_path = 'models_pipeline_timestamp_with_validation.pkl'
+file_path = 'results_pipeline_timestamp_with_validation.pkl'
 
 # Tải nội dung của file .pkl
 data = joblib.load(file_path)
